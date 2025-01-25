beb295eb37cf6b07f972fc6c0f8573bdd2e483ea
Mariana-Teixeira <teixeira.marianareis@gmail.com>
2025-01-25 11:25:17 +0000

    fix: update to unity 6000.0.10f1

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(Event.current.keyCode);
     }
 }
